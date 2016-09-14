4b89a6c4ace2eb8a13c979f8f6f4bfdacc59d23e
Varun-Garg <varun.10@live.com>
2016-09-14 21:08:11 +0100

    app: created login page

--- Client App/src/StudentManagementSystem/Main.java
+++ Client App/src/StudentManagementSystem/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package StudentManagementSystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Login to Student Management System");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
