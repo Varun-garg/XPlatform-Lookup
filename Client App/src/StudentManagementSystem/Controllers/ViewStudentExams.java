d895fd99f4472cd135deff2b3b243b6cb0954f0e
rishabh279 <rishabhahuja279@gmail.com>
2016-12-01 11:00:45 +0530

    functionalities added in exams section

--- Client App/src/StudentManagementSystem/Controllers/ViewStudentExams.java
+++ Client App/src/StudentManagementSystem/Controllers/ViewStudentExams.java
@@ -20,6 +20,7 @@ import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -60,6 +61,7 @@ public class ViewStudentExams implements Initializable {
         return SessionManager.getCookie();
     }
 
+    int deletesem;
     public void displayExamsInfo(String n) {
         WebTarget clientTarget;
         Client client = ClientBuilder.newClient();
@@ -73,6 +75,7 @@ public class ViewStudentExams implements Initializable {
             List<OverallResult> overallResult = e.getOverallResult();
             List<MarksSummary> marksSummary = e.getMarksSummary();
 
+            deletesem = overallResult.get(0).getSemester();
             l3.setText(String.valueOf("SEMESTER-" + overallResult.get(0).getSemester()));
 
             PropertyValueFactory<ExamsResult, String> subjectcodeProperty =
@@ -98,24 +101,48 @@ public class ViewStudentExams implements Initializable {
             e.printStackTrace();
         }
     }
+
+    public void totalSemesters() {
+        WebTarget clientTarget;
+        Client client = ClientBuilder.newClient();
+        clientTarget = client.target(Configuration.API_HOST + "data/student/semesters/"+rollNo+"/");
+        //clientTarget = client.target("https://studentmanagementsystem.pythonanywhere.com/data/student/semesters/13ICS047/");
+        javax.ws.rs.core.Response rawResponse = clientTarget.request("application/json").get();
+        String response = rawResponse.readEntity(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        ExamSemesterNum num ;
+
+        try {
+            num = mapper.readValue(response, ExamSemesterNum.class);
+            System.out.println(response);
+
+            List<Integer> semesters = num.getSemesters();
+            System.out.println(semesters.get(0));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     private TextField Subjectcode;
     @FXML
     private TextField Grade;
+    @FXML
+    private TextField Semester;
 
     public void addExams(ActionEvent event) throws IOException {
         Form newExamForm = new Form();
 
+        String sem= Semester.getText();
         newExamForm.param("roll_no", rollNo);
         newExamForm.param("subject_code", Subjectcode.getText());
         newExamForm.param("grade", Grade.getText());
+        newExamForm.param("semester", sem);
         newExamForm.param("tc", "0");
         newExamForm.param("tgp","0");
-        newExamForm.param("sgpa", "0");
-        newExamForm.param("result", "0");
-        newExamForm.param("semester", "0");
-
-
+        newExamForm.param("sgpa", "8");
+        newExamForm.param("result", "Promoted");
 
         WebTarget clientTarget;
         Client client = ClientBuilder.newClient();
@@ -138,14 +165,54 @@ public class ViewStudentExams implements Initializable {
                     alert.close();
                 }
             });
-            displayExamsInfo("7");
+            displayExamsInfo(sem);
 
 
-            //   this.setRollNo(roll_no1.getText());
-            //   tab_plane.getSelectionModel().select(0);
-            //   this.Display();
         } else {
             System.out.println("Error: Student with could not be added");
+            System.out.println(response);
+        }
+    }
+
+    public void deleteStudentExam(ActionEvent event) throws IOException {
+        System.out.println("inside delete hostel"+deletesem);
+        Form newExamForm = new Form();
+        String rollNo = SessionManager.getInstance().getStudentRollNo();
+        newExamForm.param("roll_no", rollNo);
+        newExamForm.param("semeste", String.valueOf(deletesem));
+
+        WebTarget clientTarget;
+        Client client = ClientBuilder.newClient();
+        clientTarget = client.target(Configuration.API_HOST + "data/student/exam/delete/"+rollNo+"/"+deletesem+"/");
+
+        javax.ws.rs.core.Response rawResponse = clientTarget.request("application/json").header("Cookie", SessionManager.getCookie()).
+                post(Entity.entity(newExamForm, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+
+        String response = rawResponse.readEntity(String.class);
+        System.out.println(response);
+        ObjectMapper mapper = new ObjectMapper();
+        PostResponse deleteExamResponse = mapper.readValue(response, PostResponse.class);
+
+        System.out.println("got message " + deleteExamResponse.getMessage());
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Message Here...");
+        if (deleteExamResponse.getMessage().equals("success")) {
+
+            alert.setContentText("Result has been successfully Deleted");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) {
+                    alert.close();
+                }
+            });
+            System.out.print("Result successfully deleted");
+
+        } else {
+            alert.setContentText("Result could not be Deleted");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) {
+                    alert.close();
+                }
+            });
             System.out.println(response);
         }
     }
@@ -154,12 +221,16 @@ public class ViewStudentExams implements Initializable {
     public void choicebox() {
         for (int i = 1; i <= 7; i++)
             cb.getItems().add("" + i);
-        cb.setValue("7");
+        cb.setValue("6");
     }
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        displayExamsInfo("7");
+
+        displayExamsInfo("6");
         this.choicebox();
+        //totalSemesters();
     }
 }
